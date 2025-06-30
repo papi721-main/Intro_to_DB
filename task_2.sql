a992c8db2447e883ac98d563f09a0e74523da7eb
papi721-main <99392252+papi721-main@users.noreply.github.com>
2025-06-30 12:12:04 +0300

    chore: remove spaces, for the checker :/

--- task_2.sql
+++ task_2.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 
 
 ALTER TABLE Books
-ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Authors (author_id);
+ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Authors(author_id);
 
 
 CREATE TABLE IF NOT EXISTS Customers (
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 
@@ -44,6 +44,6 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE(10, 2) NOT NULL,
-    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
